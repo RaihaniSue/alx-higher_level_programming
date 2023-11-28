f47150bb275c0f9dd4123a852dc1dac3b2c12eeb
RaihaniSue <sue.raih@gmail.com>
2023-11-28 14:01:29 -0800

    fifth task

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    output = f"{i:02}"
-    if i != 99:
-        output += ","
+    if i == 98:
+        print("{}".format(i))
     else:
-        output += "\n"
-    print(output, end="")
+        print("{:02}".format(i), end=",")
