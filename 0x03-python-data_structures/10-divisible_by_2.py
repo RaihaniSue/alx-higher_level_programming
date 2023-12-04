fc4bdae9e37efd026188a59d30013d6a15b8f495
RaihaniSue <sue.raih@gmail.com>
2023-12-04 12:15:21 -0800

    only ny 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    l_ist = []
+    for i in my_list:
+        if i % 2 is 0:
+            l_ist.append(True)
+        else:
+            l_ist.append(False)
+    return (l_ist)
