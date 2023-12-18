afa7623b8c4cf5244dd79514912b3a59df77f5fa
RaihaniSue <sue.raih@gmail.com>
2023-12-18 13:41:24 -0800

    exceptiondivision0

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         div_res = a / b
-    except:
+    except ZeroDivisionError:
         div_res = None
     finally:
         print("Inside result: {}".format(div_res))
